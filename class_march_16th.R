7131f9745793563c802e488f96e423cac110d4d1
mallory duy <mduy@vols.utk.edu>
2026-03-18 12:13:15 -0400

    Testing for homework

---
+++ class_march_16th.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+library(nycflights13)
+?gss_cat
+
+# logical vectors - conditonal transformations
+x <- c(-3:3, NA)
+if_else(x > 0, "+ve", "-ve")
+if_else(x > 0, "+ve", "-ve", "???")
+
+# numbers
+1:10 %/% 3
+1:10 %% 3
+
+# an example of %/% in a practical workflow
+flights |> 
+  group_by(hour = sched_dep_time %/% 100) |> 
+  summarize(prop_cancelled = mean(is.na(dep_time)), n = n()) |> 
+  filter(hour > 1) |> 
+  ggplot(aes(x = hour, y = prop_cancelled)) +
+  geom_line(color = "grey50") + 
+  geom_point(aes(size = n))
+
+# Factors
+x1 <- c("Dec", "Apr", "Jan", "Mar")
+x2 <- c("Dec", "Apr", "Jam", "Mar")
+
+sort(x1)
+
+month_levels <- c(
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+)
+
+y1 <- factor(x1, levels = month_levels)
+y1
+
+sort(y1)
+
+y2 <- fct(x2, levels = month_levels)
+
+csv <- "
+month,value
+Jan,12
+Feb,56
+Mar,12"
+
+df <- read_csv(csv)
+df
+df <- read_csv(csv, col_types = cols(month = col_factor(month_levels)))
+
+# dates and times
+
+csv <- "
+  date
+  01/02/15
+"
+#reformatting
+read_csv(csv, col_types = cols(date = col_date("%m/%d/%y")))
+read_csv(csv, col_types = cols(date = col_date("%d/%m/%y")))
+read_csv(csv, col_types = cols(date = col_date("%y/%m/%d")))
+
+# pulling out components
+datetime <- ymd_hms("2026-07-08 12:34:56")
+
+year(datetime)
+month(datetime)
+mday(datetime)
+
+yday(datetime)
+wday(datetime)
